207cb36f9ba75f963a95e2678d6c19e7911e2629
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 17:26:47 -0300

    Adicionando Next.js ao projeto pt.3

---
+++ pages/wcube.js
@@ -0,0 +1,47 @@
+function wcube() {
+  return (
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <meta lang="pt-br"/>
+    <title>Humbanew WCube | O incrível jogo dos cubos matemáticos.</title>
+    <link rel="icon" href="svg/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
+
+    <!-- build:css css/wcube_page.css -->
+
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/fonts.css"/>
+    <link rel="stylesheet" href="css/pag-inicial/cabecalho.css"/>
+    <link rel="stylesheet" href="css/pag-inicial/rodape.css"/>
+    <link rel="stylesheet" href="css/wcube/lists/lists.css"/>
+    <link rel="stylesheet" href="css/wcube/slogan/slogan.css"/>
+
+    <!-- endbuild -->
+
+  </head>
+  <body>
+    
+    <header class="cabecalho_conteiner">
+      <div class="cabecalho_cont_parent" id="btn_main">
+        <img src="svg/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Preto.svg" alt="Humbanew Logo" class="logo_part_i"/>
+        <div class="cab_conteiner_title">
+          <h1 class="text1_title">Humbanew WCube</h1>
+        <!-- <p class="text2_title" id="beta_text">Beta</p> -->
+        </div>
+      </div>
+    </header>
+
+    <section class="unote_slogan">
+      <p class="unote_text_slogan" id="slogan">O incrível jogo dos cubos matemáticos.</p>
+    </section>
+
+  </body>
+  <footer>
+    <section class="rodape">
+      <h1 class="rodape_titulo">Humbanew © 2015 - 2021</h1>
+    </section>
+  </footer>
+</html>)
+}
+
+export default wcube;
