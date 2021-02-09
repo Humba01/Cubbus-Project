2f4adeddf37bc6766a1be41e37e7eacd7b3b388e
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 20:13:32 -0300

    Adicionando ajustes para nova interface pt.26

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,21 @@ function wcube() {
     <div>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
-      <link href="/public/fonts/Ubuntu/Ubuntu-Medium.ttf" rel="stylesheet"/>
       <section>
-        <h1 style={{fontFamily: 'Ubuntu-Medium'}}>HN WCube</h1>
+        <h1 style={cabecalho.titulo}>HN WCube</h1>
       </section>
     </div>
   )
 }
 
 export default wcube;
+
+// Estilos 
+
+let cabecalho = {
+  titulo: {
+    fontFamily: 'Ubuntu',
+    backgroundColor: '#000000',
+    color: '#ffffff'
+  }
+}
