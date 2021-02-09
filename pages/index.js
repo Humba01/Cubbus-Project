330667504add5f9301ff42a396edf3081ad530f5
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 18:00:05 -0300

    Adicionando ajustes para nova interface

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import "./../css/fonts.css";
+
 function wcube() {
   return (
-    <h1>HN-WCube</h1>
+    <section style={cabecalho.setorCabecalho}>
+      <h1>HN-WCube</h1>
+    </section>
   )
 }
 
 export default wcube;
+
+const cabecalho = StyleSheet.create({
+  setorCabecalho: {
+    backgroundColor: "#000000",
+    color: "#ffffff",
+    fontFamily: "Ubuntu-Light"
+  }
+});
