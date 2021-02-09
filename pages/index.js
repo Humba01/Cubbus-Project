179077e8a493ad6e2694b1a04c8221c59173fc61
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 18:49:35 -0300

    Adicionando ajustes para nova interface pt.13

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import { React } from 'react';
 import { ReactDOM } from 'react-dom';
 import styles from './Fonts.module.css';
-import styles from './Reset.module.css';
-import styles from './Cabecalho.module.css';
+import styles2 from './Reset.module.css';
+import styles3 from './Cabecalho.module.css';
 
 function wcube() {
   return (
-    <section className={styles.cabecalho_cont}>
-      <h1 className={styles.cabecalho}>HN WCube</h1>
+    <section className={styles3.cabecalho_cont}>
+      <h1 className={styles3.cabecalho}>HN WCube</h1>
     </section>
   )
 }
