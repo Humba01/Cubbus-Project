235c5aeb3f22649c7d0447f06464ce03aa651e9e
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 19:35:31 -0300

    Adicionando ajustes para nova interface pt.15

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import { React } from 'react';
-import styles3 from './Cabecalho.module.css';
 
 function wcube() {
   return (
     <body>
       <link rel="stylesheet" href="../css/fonts.css"></link>
       <section className={styles3.cabecalho_cont}>
-        <h1 className={styles3.cabecalho}>HN WCube</h1>
+        <h1 className={{fontFamily="Ubuntu-Light"}}>HN WCube</h1>
       </section>
     </body>
   )
