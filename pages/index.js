02be2c3250e231c0c2308e01489dac6080e5ea55
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 20:07:08 -0300

    Adicionando ajustes para nova interface pt.23

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import { React } from 'react';
-import { Link } from 'next/link';
-import { Head } from 'next/head';
 
 function wcube() {
   return (
     <div>
-      <link rel="stylesheet" href="./public/fonts/Ubuntu/Ubuntu-Light.ttf"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
       <section>
         <h1 style={{fontFamily: "Ubuntu-Light"}}>HN WCube</h1>
       </section>
