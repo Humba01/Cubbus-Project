8a740da4d02c03dbcb8b217e7492a53a0791d5a6
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 20:08:10 -0300

    Adicionando ajustes para nova interface pt.24

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function wcube() {
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
       <section>
-        <h1 style={{fontFamily: "Ubuntu-Light"}}>HN WCube</h1>
+        <h1 style={{fontFamily: 'Ubuntu'}}>HN WCube</h1>
       </section>
     </div>
   )
