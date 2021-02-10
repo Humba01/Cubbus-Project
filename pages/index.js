57d1e92e5a5b58e361ac26ecdc075e72664bd20a
Humberto Bravo <humbandroid@gmail.com>
2021-02-10 01:48:15 -0300

    Arrumando as imagens da página pt.5

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { React } from 'react';
-import { Image } from 'next/image';
 
 function wcube() {
   return (
@@ -9,17 +8,12 @@ function wcube() {
         <meta charset="UTF-8"/>
         <meta lang="pt-br"/>
         <title>Humbanew WCube | O incrível jogo dos cubos matemáticos.</title>
-        <link rel="icon" href="/assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
+        <link rel="icon" href="./assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
       </head>
       <body>
         <header class="cabecalho_conteiner">
           <div class="cabecalho_cont_parent" id="btn_main">
-            <img
-              src="/svg/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Teste.svg"
-              alt="Humbanew Logo Design"
-              width='65px'
-              height='65px' 
-            />
+            <img src="./assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg" alt="Humbanew Logo" class="logo_part_i"/>
             <div class="cab_conteiner_title">
               <h1 class="text1_title">Humbanew WCube</h1>
             </div>
