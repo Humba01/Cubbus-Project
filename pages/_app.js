5141e2ce2fb92ec06ed305ada711d10355a12d7d
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 21:19:09 -0300

    Arrumando o CSS no Next.js pt.3

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../css/reset.css';
+import '../css/fonts.css';
+import '../css/pag-inicial/cabecalho.css';
+import '../css/pag-inicial/rodape.css';
+import '../css/wcube/slogan/slogan.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,39 @@ import { React } from 'react';
 
 function wcube() {
   return (
-    <html style={reset.root}>
-      <div>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
-        <section style={cabecalho.cabecalhoCont}>
-          <h1 style={cabecalho.titulo}>HN WCube</h1>
+  <div>
+    <html>
+      <head>
+        <meta charset="UTF-8"/>
+        <meta lang="pt-br"/>
+        <title>Humbanew WCube | O incrível jogo dos cubos matemáticos.</title>
+        <link rel="icon" href="svg/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
+
+      </head>
+      <body>
+        
+        <header class="cabecalho_conteiner">
+          <div class="cabecalho_cont_parent" id="btn_main">
+            <img src="svg/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Preto.svg" alt="Humbanew Logo" class="logo_part_i"/>
+            <div class="cab_conteiner_title">
+              <h1 class="text1_title">Humbanew WCube</h1>
+            </div>
+          </div>
+        </header>
+
+        <section class="unote_slogan">
+          <p class="unote_text_slogan" id="slogan">O incrível jogo dos cubos matemáticos.</p>
         </section>
-      </div>
+
+      </body>
+      <footer>
+        <section class="rodape">
+          <h1 class="rodape_titulo">Humbanew © 2015 - 2021</h1>
+        </section>
+      </footer>
     </html>
+  </div>
   )
 }
 
 export default wcube;
-
-let reset = {
-  root: {
-    // marginTop: '-10px',
-  }
-}
-
-let cabecalho = {
-  cabacalhoCont: {
-    display: 'inline-block',
-    margin: '0px',
-    width: '100vw',
-    backgroundColor: '#000000'
-  },
-  titulo: {
-    fontFamily: 'Ubuntu',
-    fontWeight: '100',
-    backgroundColor: '#6600ff',
-    color: '#ffffff'
-  }
-}
