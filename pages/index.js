ed835c1c04a5010bafa09fedefe27bd2a33a459a
Humberto Bravo <humbandroid@gmail.com>
2021-02-10 01:27:18 -0300

    Arrumando os diretórios de imagem da página pt.8

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ function wcube() {
       <body>
         <header class="cabecalho_conteiner">
           <div class="cabecalho_cont_parent" id="btn_main">
-            <img href="/svg/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
+            <img src="/assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg" alt="Humbanew Logo" class="logo_part_i"/>
             <div class="cab_conteiner_title">
               <h1 class="text1_title">Humbanew WCube</h1>
             </div>
