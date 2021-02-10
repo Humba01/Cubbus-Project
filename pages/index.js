512f3c488086e7ed269f6812d7957f820d53d4c5
Humberto Bravo <humbandroid@gmail.com>
2021-02-10 02:53:05 -0300

    Refazendo página do zero

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 
 function wcube() {
   return (
-  <div>
     <html>
       <head>
         <meta charset="UTF-8"/>
@@ -12,27 +11,11 @@ function wcube() {
         <link rel="icon" href="./assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
       </head>
       <body>
-        <header class="cabecalho_conteiner">
-          <div class="cabecalho_cont_parent" id="btn_main">
-            <img src="./assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg" alt="Humbanew Logo" class="logo_part_i"/>
-          </div>
-          <div class="cab_conteiner_title">
-            <h1 class="text1_title">Humbanew WCube</h1>
-          </div>
-        </header>
-
-        <section class="unote_slogan">
-          <p class="unote_text_slogan" id="slogan">O incrível jogo dos cubos matemáticos.</p>
-        </section>
+        
+        <header></header>
 
       </body>
-      <footer>
-        <section class="rodape">
-          <h1 class="rodape_titulo">Humbanew © 2015 - 2021</h1>
-        </section>
-      </footer>
     </html>
-  </div>
   )
 }
 
