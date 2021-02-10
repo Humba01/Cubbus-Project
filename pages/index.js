7e6362d338058cffa51b54d3030c5fccd45c36a6
Humberto Bravo <humbandroid@gmail.com>
2021-02-09 23:15:08 -0300

    Arrumando os diretórios de imagem da página pt.5

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ function wcube() {
         <meta charset="UTF-8"/>
         <meta lang="pt-br"/>
         <title>Humbanew WCube | O incrível jogo dos cubos matemáticos.</title>
-        <link rel="icon" href="./assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
+        <link rel="icon" href="/assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg"/>
       </head>
       <body>
         <header class="cabecalho_conteiner">
           <div class="cabecalho_cont_parent" id="btn_main">
-            <img src="./assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg" alt="Humbanew Logo" class="logo_part_i"/>
+            <img src="/assets/logo/new-logo/Humbanew Logo (Design) - 2.0 - 12º Gen - Branco.svg" alt="Humbanew Logo" class="logo_part_i"/>
             <div class="cab_conteiner_title">
               <h1 class="text1_title">Humbanew WCube</h1>
             </div>
